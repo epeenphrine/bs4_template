5bb309bf2435eb65e82cebab40c0c36bd2300b17
dong <donghyunwon610@gmail.com>
2020-11-19 07:12:34 +0000

    added local stuff

--- bs4_template.py
+++ bs4_template.py
@@ -1,5 +1,22 @@
 # need lxml from pip (pip3 install lxml)
 #%%
+## save requests response locally 
+import requests
+import bs4 as bs
+import json 
+
+res = requests.get('https://google.com')
+with open('data.html', 'w') as f:
+    f.write(str(res.content)) ## str() to save
+
+
+## load local html into bs4
+with open('data.html') as f:
+    soup = bs.BeautifulSoup(f, 'lxml')
+div = soup.findAll('div')
+print(div)
+
+#%%
 import bs4 as bs
 import requests
 
@@ -15,4 +32,3 @@ div_class = soup.findAll('div', {'class': 'contentRow-snippetk'})
 
 # find all a tags with href that contains text between tags  
 hrefs = soup.findAll('a', href=True, text=True) 
-
